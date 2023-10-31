e2143a61bb5c919da4058827e3c4e3c230f8f8e1
samonuall <samonuall@gmail.com>
2023-10-31 11:14:29 -0400

    add example for getting json data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import post_data from './mock/mock.json'
+import {Post} from './models/Post'
 
 function App() {
+  let post_data: Post[] = require('./mock/mock.json');
+  console.log(post_data[279].number);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          React App
         </p>
         <a
           className="App-link"
