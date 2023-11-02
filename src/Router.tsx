d325437d31e8e1f6284d608c020914c8b586ff0b
Derek <derekalacy@gmail.com>
2023-11-02 13:36:13 -0400

    added navbar to engagementpage

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TrendingPage from "./pages/trending-page/TrendingPage";
+import EngagementPage from './pages/engagement-page/engagement-page';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TrendingPage />}>
+        <Route path="/" element={<EngagementPage />}>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/engagement-page/engagement-page.tsx
+++ src/pages/engagement-page/engagement-page.tsx
@@ -1,1 +1,13 @@
-export {}
+import React from 'react';
+import './EngagementPage.css';
+import Navbar from '../../components/navbar/navbar';
+
+function EngagementPage() {
+  return (
+    <>
+        <Navbar />
+    </>
+  );
+}
+
+export default EngagementPage;
