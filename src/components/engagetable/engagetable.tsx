c959aea8324f761fe7f838f975ff66004e9dc219
Derek <derekalacy@gmail.com>
2023-11-06 22:46:01 -0500

    edited table

--- src/components/engagetable/engagetable.tsx
+++ src/components/engagetable/engagetable.tsx
@@ -8,8 +8,9 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
+//******************************* Random Data Generation *******************************
 interface Column {
-  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  id: 'name' | 'totalPosts' | 'lastSeenDaysAgo';
   label: string;
   minWidth?: number;
   align?: 'right';
@@ -17,66 +18,33 @@ interface Column {
 }
 
 const columns: readonly Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: 'name', label: 'Name', minWidth: 100 },
+  { id: 'totalPosts', label: 'Total Posts', minWidth: 100, align: 'right' },
   {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
+    id: 'lastSeenDaysAgo',
+    label: 'Last Seen (Days Ago)',
+    minWidth: 100,
     align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toFixed(2),
+    format: (value: number) => value.toString(),
   },
 ];
 
 interface Data {
   name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
+  totalPosts: number;
+  lastSeenDaysAgo: number;
 }
 
-function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number,
-): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
+function createData(name: string, totalPosts: number, lastSeenDaysAgo: number): Data {
+  return { name, totalPosts, lastSeenDaysAgo };
 }
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
+const names = ['John', 'Jane', 'Mary', 'James', 'Emma', 'Jacob', 'Olivia', 'Noah', 'Sophia', 'Mia', 'Charlotte', 'Amelia', 'Harper', 'Evelyn', 'Abigail'];
+const rows = names.map(name => createData(name, Math.floor(Math.random() * 1000), Math.floor(Math.random() * 365)));
+
+// ******* end random data generation ********
+
+
 
 export default function StickyHeadTable() {
   const [page, setPage] = React.useState(0);
@@ -101,7 +69,7 @@ export default function StickyHeadTable() {
                 <TableCell
                   key={column.id}
                   align={column.align}
-                  style={{ minWidth: column.minWidth }}
+                  style={{ minWidth: column.minWidth, fontWeight: 'bold' }}
                 >
                   {column.label}
                 </TableCell>
@@ -113,7 +81,7 @@ export default function StickyHeadTable() {
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.name}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
