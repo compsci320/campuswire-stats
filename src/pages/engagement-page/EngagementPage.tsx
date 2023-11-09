da77728fb48989f0e40d7c41f4fced9ebd2602d9
Derek <derekalacy@gmail.com>
2023-11-09 13:27:41 -0500

    edited page import

--- src/pages/engagement-page/EngagementPage.tsx
+++ src/pages/engagement-page/EngagementPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './EngagementPage.css';
 import Navbar from '../../components/navbar/navbar';
-import Engagetable from '../../components/engagetable/engagetable'
+import Engagetable from '../../components/engagetable/engagetable';
 
 function EngagementPage() {
   return (
