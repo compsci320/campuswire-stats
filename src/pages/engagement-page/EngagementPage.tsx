296b181161b150db1cb7a36d2810fcb9ff6522a3
Derek <derekalacy@gmail.com>
2023-11-10 15:04:51 -0500

    added graph

--- src/pages/engagement-page/EngagementPage.tsx
+++ src/pages/engagement-page/EngagementPage.tsx
@@ -4,6 +4,7 @@ import Navbar from '../../components/navbar/navbar';
 import { createViewModel } from '../../service/EngagementService';
 import {Post} from "../../models/Post";
 import { Engagetable, Column, Data } from '../../components/engagetable/engagetable';
+import { EngagementGraph } from "../../components/engagementGraph/engagementGraph"
 
 
 
@@ -38,6 +39,7 @@ function EngagementPage() {
   return (
     <>
         <Navbar />
+        <EngagementGraph engagement_data={[2, 5, 3, 4, 1, 3, 8, 9]} x_title={"Years"} x_axis={[2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]}/>
         <Engagetable columns={columns} rows={rows} />
     </>
   );
