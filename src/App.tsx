85688413196d6564102c433d1d93fea83118735e
liamkaigates.cs@gmail.com <liamgates19@icloud.com>
2023-11-11 23:08:53 -0500

    set up for flask

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import Router from './Router';
-import {Post} from './models/Post';
 
 function App() {
-  let post_data: Post[] = require('./mock/mock.json');
-  console.log(post_data[279].number);
-  
+
   return (
-    <RouterProvider router={Router} />    
+    <RouterProvider router={Router} />
   );
 }
 
