76b2ae18f4c8d4302248ee41bb165e2dd4ec8c16
samonuall <samonuall@gmail.com>
2023-11-30 15:44:05 -0500

    add temporary buttons for sorting

--- src/pages/engagement-page/EngagementPage.tsx
+++ src/pages/engagement-page/EngagementPage.tsx
@@ -5,6 +5,12 @@ import {Post} from "../../models/Post";
 import { Engagetable, Column, Data } from '../../components/engagetable/engagetable';
 import { EngagementGraph } from "../../components/engagementGraph/engagementGraph"
 import EngagementBar from '../../components/engagementbar/engagementbar';
+import { basename } from 'path/posix';
+
+// Stuff for sort buttons
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 
 
@@ -39,7 +45,7 @@ function createData(name: string, numPosts: string, numComments: string, lastSee
 const rows = nameArray.map((name, index) => createData(name, numPostsArray[index], numCommentsArray[index], lastSeenArray[index]));
 
 
-function compare_columns(option: string, type: string)  {
+function compare_columns(option: string, isString: boolean, ascending: boolean)  {
   // Type is either "string" or "number"
   return (a: any, b: any) => {
     a = a[option];
@@ -47,23 +53,35 @@ function compare_columns(option: string, type: string)  {
     console.log("A:", a);
     console.log("B:", b);
     
-    if (type === "string") {
-      return a.localeCompare(b);
-    } else if (type === "number") {
-      a = a === undefined ? 0 : a;
-      b = b === undefined ? 0 : b;
+    if (isString) {
+      return ascending ? a.localeCompare(b) : -a.localeCompare(b);
+    } else {
+      
+      if (a === undefined) {
+        a = 0;
+      } else {
+        a = ascending ? parseFloat(a) : -parseFloat(a);
+      }
 
-      return parseFloat(a) < parseFloat(b) ? -1 : 1;
+      if (b === undefined) {
+        b = 0;
+      } else {
+        b = ascending ? parseFloat(b) : -parseFloat(b);
+      }
+
+      return a < b ? -1 : 1;
     }
   }
 }           
 
 function EngagementPage() {
   const chartData = createEngagementChartData(post_data);
-  let [sortOption, setSortOption] = React.useState("numComments");
+  const [sortOption, setSortOption] = React.useState("");
+  const [sortAscending, setSortAscending] = React.useState(true);
 
-  if (sortOption !== "") {
-    rows.sort(compare_columns(sortOption, "number"));
+  if (sortOption.length > 0) {
+    // Assumes that name and lastSeen are strings, need to change this if more string fields are added to the rows
+    rows.sort(compare_columns(sortOption, sortOption === "name" || sortOption === "lastSeen", sortAscending));
   }
     
   return (
@@ -77,6 +95,12 @@ function EngagementPage() {
           }}>
             <EngagementGraph engagement_data={chartData.y} x_title={"Dates"} x_axis={chartData.x}/>
           </div>
+          {/* Sort button stuff */}
+          <button onClick={() => setSortOption("name")}>Sort by Name</button>
+          <button onClick={() => setSortOption("numPosts")}>Sort by Number of Posts</button>
+          <button onClick={() => setSortAscending(!sortAscending)}>{sortAscending ? "Descend" : "Ascend"}</button>
+          
+          {/* Table stuff */}
           <Engagetable columns={columns} rows={rows} />
         </div>
     </>
