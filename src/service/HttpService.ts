62369b2df5c81cf8f558b01dc6e2e78d5e2b2e7f
algispetlin <apetlin@umass.edu>
2023-12-05 12:20:21 -0500

    Disabled CORS on Frontend

--- src/service/HttpService.ts
+++ src/service/HttpService.ts
@@ -2,7 +2,8 @@ const API_URL = 'http://localhost:5000';
 
 export async function get(path: string) {
     const requestInstructions = { 
-        method: 'GET'
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
     }
 
     return await fetch(API_URL + path, requestInstructions)
@@ -12,7 +13,7 @@ export async function get(path: string) {
 export async function post(path: string, payload: any) {
     const requestInstructions = { 
         method: 'POST', 
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
         body: JSON.stringify(payload)
     }
 
